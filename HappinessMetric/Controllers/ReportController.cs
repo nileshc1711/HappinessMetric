eb218c7b9806952d4675af2496dfdbb1c1e7643d
Nilesh Chaudhary <Nileshc@medusind.com>
2017-07-25 18:21:58 +0530

    adding files

---
+++ HappinessMetric/Controllers/ReportController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HappinessMetric.Controllers
+{
+    public class ReportController : Controller
+    {
+        //
+        // GET: /Report/
+
+        public ActionResult UserReport()
+        {
+            return View();
+        }
+
+    }
+}
